94f16988272bb6142b0cb7f87750b13487a895c4
Manojnagula <manojnagula44@gmail.com>
2023-10-28 12:48:34 +0530

    added login & signup buttons

--- CLIENT/src/layouts/HomeLayout.jsx
+++ CLIENT/src/layouts/HomeLayout.jsx
@@ -1,9 +1,17 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Footer from "../components/Footer";
 import { FiMenu } from "react-icons/fi";
 import { AiFillCloseCircle } from "react-icons/ai";
+import { useDispatch, useSelector } from "react-redux";
 
 function HomeLayout({ children }) {
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const isLoggedIn = useSelector((state)=>state?.auth?.isLoggedIn);
+  const role = useSelector((state)=>state?.auth?.role)
+
   function changeWidth() {
     const drawerSide = document.getElementsByClassName("drawer-side");
     drawerSide[0].style.width = "auto";
@@ -15,6 +23,16 @@ function HomeLayout({ children }) {
     const drawerSide = document.getElementsByClassName("drawer-side");
     drawerSide[0].style.width = "0";
   }
+
+  function onLogout(e){
+//todo
+
+e.preventDefault();
+
+//todo
+navigate("/")
+  }
+
   return (
     <div className="min-h-[90vh]">
       <div className="drawer absolute left-0 z-50 w-full">
@@ -39,6 +57,11 @@ function HomeLayout({ children }) {
             <li>
               <Link to="/">Home</Link>
             </li>
+            {isLoggedIn && role === 'ADMIN' && (
+              <li>
+                <Link to="/admin/dashboard">Admin Dashboard</Link>
+              </li>
+            )}
             <li>
               <Link to="/about">About us</Link>
             </li>
@@ -48,6 +71,32 @@ function HomeLayout({ children }) {
             <li>
               <Link to="/courses">All Courses</Link>
             </li>
+
+            { !isLoggedIn? (
+              <li className="absolute bottom-4 w-[90%]">
+                <div className="w-full flex items-center justify-center">
+                  <button className="btn-primary px-4 py-1 font-semibold rounded-md w-full ">
+                    <Link to="/login">Login</Link>
+                  </button>
+                  <button className="btn-secondary px-4 py-1 font-semibold rounded-md w-full ">
+                    <Link to="/login">Signup</Link>
+                  </button>
+                </div>
+              </li>
+            ) : (
+              <li className="absolute bottom-4 w-[90%]">
+              <div className="w-full flex items-center justify-center">
+                <button className="btn-primary px-4 py-1 font-semibold rounded-md w-full ">
+                  <Link to="/user/profile">profile</Link>
+                </button>
+                <button className="btn-secondary px-4 py-1 font-semibold rounded-md w-full ">
+                  <Link onClick={onLogout}>Logout</Link>
+                </button>
+              </div>
+            </li>
+            )
+          }
+
           </ul>
         </div>
       </div>
