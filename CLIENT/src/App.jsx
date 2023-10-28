3278c4e414202be5d2bf3a8d1bb94dbea686692b
Manojnagula <manojnagula44@gmail.com>
2023-10-28 12:01:56 +0530

    added basic layout and integrated to home page

--- CLIENT/src/App.jsx
+++ CLIENT/src/App.jsx
@@ -1,14 +1,16 @@
 import './App.css'
 
-import Footer from './components/Footer'
+import {Route, Routes} from 'react-router-dom'
+
+import Home from './pages/Home'
+
 
 function App() {
 
   return (
-    
-  <>
-  hi
-  </>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+    </Routes>
     )
 }
 
