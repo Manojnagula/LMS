6def8abd433804ce542d361554f08aa1cd5577d5
Manojnagula <manojnagula44@gmail.com>
2023-11-13 21:38:51 +0530

    edit profile ui left

--- CLIENT/src/pages/User/EditProfile.jsx
+++ CLIENT/src/pages/User/EditProfile.jsx
@@ -3,60 +3,64 @@ import HomeLayout from "../../layouts/HomeLayout";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import toast from "react-hot-toast";
+import { getUserData, updateProfile } from "../../redux/slices/authSlice";
 
-function EditProfile(){
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
+function EditProfile() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    const [data, setData] = useState({
-        fullName:'',
-        previewImage:'',
-        avatar:undefined,
-        userId : useSelector((state)=> state?.auth?.data?.user?._id)
-    });
+  const [data, setData] = useState({
+    fullName: "",
+    previewImage: "",
+    avatar: undefined,
+    userId: useSelector((state) => state?.auth?.data?.user?._id),
+  });
 
-    function handleImageUplaod(e){
-        e.preventDeadfault();
-        const uploadedImage = e.target.files[0];
-        if(uploadedImage){
-            const fileReader = new fileReader();
-            fileReader.readAsDataURL(uploadedImage)
-            fileReader.addEventListener('load', function(){
-                setData({
-                    ...data,
-                    previewImage: this.result, //base64 url
-                    avatar:uploadedImage //actual file
-                })
-            })
-        }
-    }
-
-    function handleInputChange(e){
-        const {name, value} = e.target;
+  function handleImageUplaod(e) {
+    e.preventDeadfault();
+    const uploadedImage = e.target.files[0];
+    if (uploadedImage) {
+      const fileReader = new fileReader();
+      fileReader.readAsDataURL(uploadedImage);
+      fileReader.addEventListener("load", function () {
         setData({
-            ...data,
-            [name] : value
-            
+          ...data,
+          previewImage: this.result, //base64 url
+          avatar: uploadedImage, //actual file
         });
+      });
+    }
+  }
+
+  function handleInputChange(e) {
+    const { name, value } = e.target;
+    setData({
+      ...data,
+      [name]: value,
+    });
+  }
+
+  async function onFormSubmit(e) {
+    e.preventDeadfault();
+    if (!data.fullName || !data.avatar) {
+      toast.error("All fields are mandatory");
+      return;
+    }
+    if (data.fullName.length < 5) {
+      toast.error("FullName shouldn't be lessthan 5 characters.");
+      return;
     }
 
-    onFormSubmit(e){
-        e.preventDeadfault();
-        if(!data.fullName || !data.avatar){
-            toast.error("All fields are mandatory");
-        }
-        if(data.fullName.length < 5 ){
-            toast.error("FullName shouldn't be lessthan 5 characters.");
-            return
-        }
+    const formData = new FormData();
+    formData.append("fullName", data.fullName);
+    formData.append("avatar", data.avatar);
+    await dispatch(updateProfile([data.userId, formData])); //All the async thunk we implement expects only one argument, Thatswhy passing an array instead of two arguments seperately.
+    await dispatch(getUserData());
 
-    }
+    navigate("/user/profile");
+}
 
-    return(
-        <HomeLayout>
-
-        </HomeLayout>
-    )
+  return <HomeLayout></HomeLayout>;
 }
 
 export default EditProfile;
--- CLIENT/src/redux/slices/authSlice.js
+++ CLIENT/src/redux/slices/authSlice.js
@@ -1,91 +1,129 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import toast from "react-hot-toast";
-import axiosInstance from '../../config/axiosinstance'
+import axiosInstance from "../../config/axiosinstance";
 
 const initialState = {
-    isLoggedin: localStorage.getItem("isLoggedin") || false,
-    role : localStorage.getItem("role") || "",
-    data: JSON.parse(localStorage.getItem("data")) || {}
-}
-
+  isLoggedin: localStorage.getItem("isLoggedin") || false,
+  role: localStorage.getItem("role") || "",
+  data: JSON.parse(localStorage.getItem("data")) || {},
+};
 
 // Actions
 
-export const createAccount = createAsyncThunk("/auth/signup", async (data)=> {
-    try {
-        const response = axiosInstance.post("users/register",data);
-        toast.promise(response, {
-            loading: 'Wait! creating your account',
-            success:(data)=>{
-                return data?.data?.message;
-            },
-            error:'Failed to create your account'
-        });
-        
-        return await response;
-    } catch (error) {
-        console.log(error);
-        toast.error(error?.response?.data?.message)
-    }
-})
-export const login = createAsyncThunk("/auth/signin", async (data)=> {
-    try {
-        const response = axiosInstance.post("users/login",data);
-        toast.promise(response, {
-            loading: 'Authenticating, please wait!',
-            success:(data)=>{
-                return data?.data?.message;
-            },
-            error:'User authentication failed.'
-        });
-        
-        return await response;
-    } catch (error) {
-        console.log(error);
-        toast.error(error?.response?.data?.message)
-    }
-})
-export const logout = createAsyncThunk("/auth/logout", async ()=> {
-    try {
-        const response = axiosInstance.get("users/logout");
-        toast.promise(response, {
-            loading: 'Logging out...',
-            success:(data)=>{
-                return data?.data?.message;
-            },
-            error:'Failed to logout.'
-        });
-        
-        return await response;
-    } catch (error) {
-        console.log(error);
-        toast.error(error?.response?.data?.message)
-    }
-})
+export const createAccount = createAsyncThunk("/auth/signup", async (data) => {
+  try {
+    const response = axiosInstance.post("users/register", data);
+    toast.promise(response, {
+      loading: "Wait! creating your account",
+      success: (data) => {
+        return data?.data?.message;
+      },
+      error: "Failed to create your account",
+    });
 
+    return await response;
+  } catch (error) {
+    console.log(error);
+    toast.error(error?.response?.data?.message);
+  }
+});
+export const updateProfile = createAsyncThunk(
+  "/auth/updateprofile",
+  async (data) => {
+    try {
+      const response = axiosInstance.put(`users/update/${data}`, data[1]);
+      toast.promise(response, {
+        loading: "Wait! Upadating your account",
+        success: (data) => {
+          return data?.data?.message;
+        },
+        error: "Failed to update your account",
+      });
+
+      return (await response).data;
+    } catch (error) {
+      console.log(error);
+      toast.error(error?.response?.data?.message);
+    }
+  }
+);
+export const getUserData = createAsyncThunk("/auth/getData", async () => {
+  try {
+    const response = axiosInstance.get("/user/me");
+    return (await response).data;
+  } catch (error) {
+    console.log(error);
+    toast.error(error?.message);
+  }
+});
+
+export const login = createAsyncThunk("/auth/signin", async (data) => {
+  try {
+    const response = axiosInstance.post("users/login", data);
+    toast.promise(response, {
+      loading: "Authenticating, please wait!",
+      success: (data) => {
+        return data?.data?.message;
+      },
+      error: "User authentication failed.",
+    });
+
+    return await response;
+  } catch (error) {
+    console.log(error);
+    toast.error(error?.response?.data?.message);
+  }
+});
+export const logout = createAsyncThunk("/auth/logout", async () => {
+  try {
+    const response = axiosInstance.get("users/logout");
+    toast.promise(response, {
+      loading: "Logging out...",
+      success: (data) => {
+        return data?.data?.message;
+      },
+      error: "Failed to logout.",
+    });
+
+    return await response;
+  } catch (error) {
+    console.log(error);
+    toast.error(error?.response?.data?.message);
+  }
+});
 
 // this is reducer function
 const authSlice = createSlice({
-    name: "auth",
-    initialState,
-    reducers:{},
-    extraReducers: (builder)=>{
-        builder
-        .addCase(login.fulfilled, (state,action)=>{
-            localStorage.setItem("data", JSON.stringify(action?.payload?.data));
-            localStorage.setItem("isLoggedin",true)
-            localStorage.setItem("role",action?.payload?.data?.user?.role)
-            state.isLoggedin = true;
-            state.role = action?.payload?.data?.user?.role;
-            state.data = action?.payload?.data
-        })
-        .addCase(logout.fulfilled, (state)=>{
-            localStorage.clear();
-            state.isLoggedin = false;
-            state.role = "";
-            state.data = ""
-        })
-    }
+  name: "auth",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(login.fulfilled, (state, action) => {
+        localStorage.setItem("data", JSON.stringify(action?.payload?.data));
+        localStorage.setItem("isLoggedin", true);
+        localStorage.setItem("role", action?.payload?.data?.user?.role);
+        state.isLoggedin = true;
+        state.role = action?.payload?.data?.user?.role;
+        state.data = action?.payload?.data;
+      })
+      .addCase(logout.fulfilled, (state) => {
+        localStorage.clear();
+        state.isLoggedin = false;
+        state.role = "";
+        state.data = "";
+      })
+      .addCase(getUserData.fulfilled, (state, action)=>{
+        if(!action?.payload?.data) return;
+        localStorage.setItem("data", JSON.stringify(action?.payload?.user));
+        localStorage.setItem("isLoggedin", true);
+        localStorage.setItem("role", action?.payload?.user?.role);
+        state.isLoggedin = true;
+        state.role = action?.payload?.user?.role;
+        state.data = action?.payload?.user;
+
+      })
+  },
 });
 
 export default authSlice.reducer;
